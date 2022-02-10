2dadaab9276d1438bee02302492b8b619d5f74d8
orhanbekcioglu <orhanbekcioglu@gmail.com>
2022-02-10 22:54:48 +0300

    Add files via upload
    Project Start

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public int Title { get; set; }
+        public int Description { get; set; }
+        public int Image1 { get; set; }
+        public int Title2 { get; set; }
+        public int Description2 { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
